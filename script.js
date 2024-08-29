ad689c0543f63387f5ae9097cd138293b6c2d341
Priyank Bhadouriya <136643402+Priyank922@users.noreply.github.com>
2024-08-30 00:02:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+
+let playerScore = 0;
+let computerScore = 0;
+const winningScore = 1;
+
+
+const playerScoreEl = document.getElementById('your-score');
+// const computerScoreEl = document.getElementById('computer-score');
+const resultText = document.getElementById('result-text');
+
+
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    console.log(computerSelection);
+    const result = getResult(playerSelection, computerSelection);
+    console.log(result);
+    if (result === 'win') {
+        playerScore++;
+    } else if (result === 'lose') {
+        computerScore++;
+    }
+    else{
+        window.location.href = 'tie.html'; 
+
+    }
+
+    updateScores();
+
+    if (playerScore === winningScore || computerScore === winningScore) {
+        setTimeout(() => endGame(), 1000); 
+    }
+}
+
+
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+
+function getResult(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'draw';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+        return 'win';
+    } else {
+        return 'lose';
+    }
+}
+
+
+function updateScores() {
+    playerScoreEl.textContent = playerScore;
+    ///computerScoreEl.textContent = computerScore;
+}
+
+
+function endGame() {
+    if (playerScore === computerScore) {
+        window.location.href = 'tie.html'; 
+    } else if (playerScore > computerScore) {
+        window.location.href = 'winning.html'; 
+    } else {
+        window.location.href = 'youwin.html'; 
+    }
+}
+
+
+
+
+document.getElementById('rules-btn').addEventListener('click', function() {
+    document.getElementById('popup').classList.add('show');
+});
+
+document.getElementById('close-popup').addEventListener('click', function() {
+    document.getElementById('popup').classList.remove('show');
+});
